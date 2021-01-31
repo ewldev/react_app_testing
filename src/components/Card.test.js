fb50db6dfcc63c7346835dc3d1dd1b450e89c565
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-01-31 01:53:50 -0500

    setup snapshot testing

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,7 +1,7 @@
-import { shallow, mount, render } from 'enzyme';
 import React from 'react';
+import { shallow } from 'enzyme';
 import Card from './Card';
 
-it ('expect to render Card component', () => {
-    expect(shallow(<Card />).length).toEqual(1)
-})    
+it('expect to render Card component', () => {
+  expect(shallow(<Card />)).toMatchSnapshot();
+});
