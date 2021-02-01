2d9bc00b0df99679d7931dfd965953f3e8ebc511
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-01-31 22:25:08 -0500

    removed node_modules folder and ran npm install, to fix coverage result not working properly, probably caused by running both npm and yarn

--- src/components/CardList.test.js
+++ src/components/CardList.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CardList from './CardList';
-const filteredRobots = [{
-    id: 1,
-    name: 'Leanne Graham',
-    username: 'Bret',
-    email: 'Sincere@april.biz'
-  }]
 
-it('expect to render CardList component', () => {
-  expect(shallow(<CardList robots={filteredRobots}/>)).toMatchSnapshot();
-});
+const mockRobots = [{
+    id: 1,
+    name: 'John Test',
+    username: 'Johnt',
+    email: 'johnt@test.com'
+}]
+
+it ('expect to render CardList component', () => {
+    expect(shallow(<CardList robots={mockRobots}/>)).toMatchSnapshot();
+})
